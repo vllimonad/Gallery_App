61849bdab5cde2b93c17afbb96d0f35da8f58521
Vlad Klunduk <klundukvlad40@gmail.com>
2024-11-28 18:46:07 +0100

    Added UICollectionView setup and UICollectionViewFlowLayout setup

--- Gallery_App/Gallery_App/ImageCollectionViewController.swift
+++ Gallery_App/Gallery_App/ImageCollectionViewController.swift
@@ -9,13 +9,41 @@ import UIKit
 
 class ImageCollectionViewController: UIViewController {
     var viewModel: ImageCollectionViewModel!
+    var collectionView: UICollectionView!
+    var layout: UICollectionViewFlowLayout!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = ImageCollectionViewModel()
         viewModel.fetchImages()
+        setupLayout()
+        setupCollectionView()
     }
-
-
+    
+    func setupLayout() {
+        layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.frame.width/3 - 2, height: view.frame.width/3 - 2)
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
+    }
+    
+    func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        collectionView.register(ImageViewCell.self, forCellWithReuseIdentifier: "ImageCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view = collectionView
+    }
 }
 
+extension ImageCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageViewCell
+        return cell
+    }
+}
