8bf5776e7e5b94569ca87f513b53a1e05601949e
Vlad Klunduk <klundukvlad40@gmail.com>
2024-12-03 15:02:15 +0100

    Added heartImageView in ImageViewCell

--- Gallery_App/Gallery_App/Views/ImageViewCell.swift
+++ Gallery_App/Gallery_App/Views/ImageViewCell.swift
@@ -8,16 +8,41 @@
 import UIKit
 
 class ImageViewCell: UICollectionViewCell {
-    private var imageView = UIImageView()
+    private var contentImageView = UIImageView()
+    private var heartImageView = {
+        let heart = UIImageView()
+        heart.image = UIImage(systemName: "heart.fill")
+        heart.tintColor = .white
+        heart.layer.opacity = 0
+        heart.translatesAutoresizingMaskIntoConstraints = false
+        return heart
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(imageView)
-        imageView.frame = contentView.bounds
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        addSubview(contentImageView)
+        addSubview(heartImageView)
+        contentImageView.frame = contentView.bounds
+        NSLayoutConstraint.activate([
+            heartImageView.leadingAnchor.constraint(equalTo: contentImageView.leadingAnchor, constant: 10),
+            heartImageView.bottomAnchor.constraint(equalTo: contentImageView.bottomAnchor, constant: -10)
+        ])
+    }
+    
+    func markAsFavourite() {
+        heartImageView.layer.opacity = 1
+    }
+    
+    func removeFromFavourite() {
+        heartImageView.layer.opacity = 1
     }
 }
 
@@ -27,7 +52,7 @@ extension ImageViewCell {
             switch result {
             case .success(let data):
                 DispatchQueue.main.async {
-                    self.imageView.image = UIImage(data: data)
+                    self.contentImageView.image = UIImage(data: data)
                 }
             case .failure(let error):
                 print(error)
