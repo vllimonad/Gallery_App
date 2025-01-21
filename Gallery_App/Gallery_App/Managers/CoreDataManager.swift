b507a85093031040d38529e96f92be69052ebd63
Vlad Klunduk <klundukvlad40@gmail.com>
2025-01-21 19:13:16 +0100

    Integrated CoreData

---
+++ Gallery_App/Gallery_App/Managers/CoreDataManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataManager.swift
+//  Gallery_App
+//
+//  Created by Vlad Klunduk on 21/01/2025.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+protocol CoreDataManagerProtocol {
+    
+}
+
+final class CoreDataManager {
+    private var context = UIApplication.shared.delegate as! AppDelegate
+    
+}
